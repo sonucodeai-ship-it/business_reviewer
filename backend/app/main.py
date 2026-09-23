1e7321d7cdf405bc87761f79cdf3892fdc3aa835
sonu-software <sonu.softwaresolution@gmail.com>
2026-09-24 00:00:46 +0530

    Improve middleware cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -22,6 +22,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://ai-reviewer-0ssx.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
